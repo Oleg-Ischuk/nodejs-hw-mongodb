791cd4a5b9807b399d93b2855a1070944a310291
Oleg_Ischuk <ipz231_ios@student.ztu.edu.ua>
2025-05-09 11:57:14 +0300

    add status:200 в деяких місцях

--- src/server.js
+++ src/server.js
@@ -31,6 +31,7 @@ export const setupServer = async () => {
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
     res.status(200).json({
+      status: 200,
       message: 'Successfully found contacts!',
       data: contacts,
     });
@@ -46,6 +47,7 @@ export const setupServer = async () => {
       });
     }
     res.status(200).json({
+      status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
